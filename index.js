08fc26f30b4de72a403bd5770a7c780a928c2ed9
Sawyer Silfies <ssilfies17@gmail.com>
2022-03-14 00:02:21 -0700

    Further class development.

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const Employee = require("./lib/Employee");
-// const Manager = require("./Manager");
+const Manager = require("./lib/Manager");
 // const Engineer = require("./Engineer");
 // const Intern = require("./Intern");
 
-const employee = new Employee();
+const teamMember = new Employee();
 
-employee.getName();
-employee.getId();
-employee.getEmail();
-employee.getRole();
+async function memberInfo () {
+    console.log('compiling member info');
 
+    const memberName = await teamMember.getName();
+    console.log(memberName);
+    const memberId = await teamMember.getId();
+    console.log(memberId);
+    const memberEmail = await teamMember.getEmail();
+    console.log(memberEmail);
+    const memberRole = await teamMember.getRole();
 
+    if (memberRole === 'Manager') {
+        const manager = new Manager();
+    }
+};
+
+memberInfo();
