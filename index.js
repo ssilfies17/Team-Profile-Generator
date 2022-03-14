ab2c9be795014d9a83fd5cab7250d1c0342b7803
Sawyer Silfies <ssilfies17@gmail.com>
2022-03-13 22:36:22 -0700

    Stuck on calling methods.

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ const Employee = require("./lib/Employee");
 // const Engineer = require("./Engineer");
 // const Intern = require("./Intern");
 
+const employee = new Employee();
+
+employee.getName();
+employee.getId();
+// employee.getEmail();
+// employee.getRole();
 
 
