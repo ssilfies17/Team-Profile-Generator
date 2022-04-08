935ac905962f399d7beef32f2a42755e98643a84
Sawyer Silfies <ssilfies17@gmail.com>
2022-04-07 23:10:54 -0700

    Employee test changes

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,5 +1,15 @@
 const Employee = require("../lib/Employee");
 
-describe("Employee", () => {
-    let employee;
+const employee = new Employee("Sawyer", 456, "ssilfies17@gmail.com");
+
+describe("Employee class", () => {
+    
+    describe("getName method", () => {
+        it("returns the name of the employee", () => {
+            expect(employee.getName()).toBe("Tara");
+        });
+    });
+
+
+
 });
