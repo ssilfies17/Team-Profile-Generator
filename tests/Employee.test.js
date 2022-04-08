414d6c0495bc04403061d9f8d5f505dc527d9e85
Sawyer Silfies <ssilfies17@gmail.com>
2022-04-07 23:16:09 -0700

    Employee test development and README changes

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -3,13 +3,29 @@ const Employee = require("../lib/Employee");
 const employee = new Employee("Sawyer", 456, "ssilfies17@gmail.com");
 
 describe("Employee class", () => {
-    
-    describe("getName method", () => {
+
+    describe("getName", () => {
         it("returns the name of the employee", () => {
-            expect(employee.getName()).toBe("Tara");
+            expect(employee.getName()).toBe("Sawyer");
         });
     });
 
+    describe("getId", () => {
+        it("returns the ID number of the employee", () => {
+            expect(employee.getId()).toBe(456);
+        });
+    });
 
+    describe("getEmail", () => {
+        it("returns the email of the employee", () => {
+            expect(employee.getEmail()).toBe("ssilfies17@gmail.com");
+        });
+    });
+
+    describe("getRole", () => {
+        it("returns the role of the employee", () => {
+            expect(employee.getRole()).toBe("Employee");
+        });
+    });
 
 });
