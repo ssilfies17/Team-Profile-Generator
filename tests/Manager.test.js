ab86af6aae74aef5eaaa4d82d8908ee3358473e6
Sawyer Silfies <ssilfies17@gmail.com>
2022-04-09 14:14:18 -0700

    Test update

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,10 +1,10 @@
 const Manager = require("../lib/Manager");
 
-const employee = new Manager(123);
+const employee = new Manager("Sawyer", 456, "ssilfies17@gmail.com", 123);
 
 describe("Manager", () => {
 
-    describe("getOffice", () => {
+    describe("getOfficeNumber", () => {
         it("returns the manager's office number", () => {
             expect(employee.getOfficeNumber()).toBe(123);
         });
